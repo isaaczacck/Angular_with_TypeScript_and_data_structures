e8bfd5090741cbc2c352da434e4e0ce55d248641
isaaczacck <isaacjohnelnar@gmail.com>
2024-10-09 02:18:36 +0800

    43.meal-plan-list

---
+++ src/app/components/meal-plan-list/meal-plan-list.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meal-plan-list',
+  templateUrl: './meal-plan-list.component.html',
+  styleUrls: ['./meal-plan-list.component.css']
+})
+export class MealPlanListComponent {
+  mealItems: string[] = [
+    'Breakfast: Oatmeal',
+    'Lunch: Chicken with rice',
+    'Dinner: Carbonara',
+    'Snack: Bread'
+  ];
+
+  newMeal: string = '';
+
+  addMeal() {
+    if (this.newMeal.trim()) {
+      this.mealItems.push(this.newMeal);
+      this.resetInputs();
+    }
+  }
+
+  deleteMeal(meal: string) {
+    this.mealItems = this.mealItems.filter(item => item !== meal);
+  }
+
+  resetInputs() {
+    this.newMeal = '';
+  }
+}
